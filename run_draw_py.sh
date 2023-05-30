2a196c099ae47748aa6825e393d5a6c6c12f6a79
Tianyi Wang <135043614+tianyiWangGithub@users.noreply.github.com>
2023-05-30 23:56:09 +0800

    Add files via upload

---
+++ run_draw_py.sh
@@ -0,0 +1,6 @@
+python3 /home/wty/ros2_dashing/src/synchronizer/scripts/N_channel.py
+python3 /home/wty/ros2_dashing/src/synchronizer/scripts/C_bound.py
+python3 /home/wty/ros2_dashing/src/synchronizer/scripts/P_period.py
+python3 /home/wty/ros2_dashing/src/synchronizer/scripts/Tw_Tb.py
+python3 /home/wty/ros2_dashing/src/synchronizer/scripts/B_bound_between.py
+python3 /home/wty/ros2_dashing/src/synchronizer/scripts/T_length.py
